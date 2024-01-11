30f6f638887c1be6534f337e4bb2268ee3ae4864
yanisapths <yanisa.p@arise.com>
2024-01-11 13:45:49 +0700

    first commit

---
+++ app/login/page.tsx
@@ -0,0 +1,17 @@
+import AcmeLogo from "../ui/acme-logo";
+import LoginForm from "../ui/login-form";
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full flex-col max-w-[400px] space-y-2.5">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
+}
